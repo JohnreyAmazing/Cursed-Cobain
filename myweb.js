8b9d20572a03bf0d46d4f209c233e893cf6010b0
JohnreyAmazing <johnreylampago@gmail.com>
2025-02-06 01:28:02 +0800

    First Commit
    6/2/2025

---
+++ myweb.js
@@ -0,0 +1,51 @@
+const scaryGif =  document.getElementById("jumpscares");
+const myButton1 = document.getElementById("button1");
+const myButton2 = document.getElementById("reloadBtn");
+const text = document.getElementById("text");
+const sound = document.getElementById("sound");
+const nirvana = document.getElementById("nirvana");
+const unmuted = document.getElementsByClassName("unmuted");
+const muted = document.getElementsByClassName("muted");
+
+nirvana.play();
+for (let i = 0; i < unmuted.length; i++) {
+    unmuted[i].onclick = function() {
+        unmuted[i].style.display = "none";
+        muted[i].style.display = "block";
+        nirvana.pause();
+    };
+};
+for (let i = 0; i < unmuted.length; i++) {
+    muted[i].onclick = function() {
+        muted[i].style.display = "none";       
+        unmuted[i].style.display= "block";
+        nirvana.play();
+    };
+};
+
+myButton1.onclick = function(){
+    nirvana.pause()
+    myButton1.remove()
+    setTimeout(() => {
+        sound.play();
+    }, 2600)
+    setTimeout(() => {
+        scaryGif.style.display = "block";
+        if(scaryGif.requestFullscreen){
+            scaryGif.requestFullscreen()
+        }
+        else if(scaryGif.webkitRequestFullscreen){
+            scaryGif.webkitRequestFullscreen()
+        }
+        else if(scaryGif.msRequestFullscreen){
+            scaryGif.msRequestFullscreen()
+        }
+        setTimeout(() => {
+            myButton2.style.display = "block"
+        }, 5000);
+    }, 3000)
+}
+scaryGif.onclick = function() {
+    window.location.reload();
+    window.scrollTo(0, 0);
+}
